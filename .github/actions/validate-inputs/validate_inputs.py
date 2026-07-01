7c8130d1139ac8e41aef73374e5c31f34625b194
Vitaliy P <vpr@lido.fi>
2026-07-01 12:03:35 +0300

    Added exclude for registry user name in validate_inputs.py

--- .github/actions/validate-inputs/validate_inputs.py
+++ .github/actions/validate-inputs/validate_inputs.py
@@ -8,33 +8,41 @@ import re
 
 re_string = r"^[\d\w.,\-/]+$"
 
+input_patterns = {
+    "registry_username": r"^[\d\w.,\-/\$]+$",
+}
+
 
 def run():
     inputs = json.loads(os.getenv("INPUTS", ""))
     if not inputs:
         return
+
     for key, value in inputs.items():
         if isinstance(value, str):
-            match = validate_string(value)
+            pattern = input_patterns.get(key, re_string)
+            match = validate_string(value, pattern)
             if not match:
                 raise ValueError(
-                    f"The value of input '{key}' isn't match regex {re_string} "
+                    f"The value of input '{key}' isn't match regex {pattern} "
                     f"and possibly has insecure chars in it"
                 )
 
 
-def validate_string(s):
+def validate_string(s, pattern=re_string):
     """
     >>> validate_string("feature/my_branch")
     True
     >>> validate_string("tags/1.2.3-dev")
     True
-    >>> validate_string("tags/1.2.3-dev")
-    True
     >>> validate_string('zzz";echo${IFS}"hello";#')
     False
+    >>> validate_string("robot$team-si-rw")
+    False
+    >>> validate_string("robot$team-si-rw", r"^[\\d\\w.,\\-/\\$]+$")
+    True
     """
-    return bool(re.match(re_string, s))
+    return bool(re.match(pattern, s))
 
 
 if __name__ == "__main__":
